dad862c4b6738b57502defa0d78b3c961bcd55ee
Wojjy72 <wojtek.graj.2004@gmail.com>
2020-09-27 12:16:10 +0200

    Add evaluation of nodes to get result

--- expression_engine/expression_engine.c
+++ expression_engine/expression_engine.c
@@ -2,7 +2,7 @@
 #include "math_functions.h"
 
 //Possible improvement: have multiple different node structures
-//multipurpose node structure. types: 'n' number, 'b' binary operation
+//multipurpose node structure. types: 'n' number, '1' L1 binary operation, '2' L2 binary operation
 typedef struct Node {
 	char type;
 	double *val_l; //pointer to (double)val of left Node
@@ -11,9 +11,12 @@ typedef struct Node {
 	double val;
 } Node;
 
-void eval_binary(Node *node)
+void eval_node(Node *node)
 {
-	node->val = (node->bin_op)(*(node->val_l), *(node->val_r));
+	double result = (node->bin_op)(*(node->val_l), *(node->val_r));
+	node->val = result;
+	*(node->val_l) = result;
+	*(node->val_r) = result;
 }
 
 void tokenize(char (*tokens)[TOKEN_AMOUNT][TOKEN_LENGTH], char input[])
@@ -40,28 +43,57 @@ void get_tokens_size(char (*tokens)[TOKEN_AMOUNT][TOKEN_LENGTH], int *tokens_siz
 	}
 }
 
-bool in_array(char value, const char *array)
+bool in_array(char value, const char *array, const int length)
 {
 	int i;
-	for(i = 0; i < sizeof(array) / sizeof(char); i++)
+	for(i = 0; i < length; i++)
 	{
 		if(value == array[i]) return true;
 	}
 	return false;
 }
 
-char get_node_type(char *value)
+void set_node_type(Node *node, char *value)
 {
-	if(in_array(value[0], OPERATIONS)) {
-		return 'b';
+	if(in_array(value[0], L1BINOPS, OPSLEN[0])) {
+		node->type = '1';
+	} else if(in_array(value[0], L2BINOPS, OPSLEN[1])){
+		node->type = '2';
 	} else {
-		return 'n';
+		node->type = 'n';
 	}
 }
 
+void assign_node_values(Node *node, double *val_l, double *val_r, double val) //double (*bin_op)(double, double)
+{
+	node->val_l = val_l;
+	node->val_r = val_r;
+	node->bin_op = NULL;
+	node->val = val;
+}
+
+void set_binary_operation(Node *node, char value)
+{
+	switch(value) {
+		case '+':
+			node->bin_op = *add;
+			break;
+		case '-':
+			node->bin_op = *subtract;
+			break;
+		case '*':
+			node->bin_op = *multiply;
+			break;
+		case '/':
+			node->bin_op = *divide;
+			break;
+	}
+}
+
+
 int main()
 {
-	char input[] = "1 + 22";
+	char input[] = "3 + 4 * 2";
 	int i;
 
 	//get tokens from input and store in smallest possible array
@@ -77,12 +109,39 @@ int main()
 	}
 	free(tokensbuf);
 
-	//create array of nodes and TODO:fill their parameters
-	Node (*nodes)[tokens_size[0]] = malloc(tokens_size[0] * sizeof(Node));
+	//create array of nodes and fill their parameters
+	Node (*nodes)[tokens_size[0]] = malloc(1000); //TODO: change from array to linked list
 	for(i = 0; i < tokens_size[0]; i++)
 	{
-		nodes[i]->type = get_node_type((*tokens)[i]);
+		char *value = (*tokens)[i];
+		set_node_type(nodes[i], value);
+		if(nodes[i]->type == 'n') {
+			assign_node_values(nodes[i], NULL, NULL, atof(value));
+		}
+		if(nodes[i]->type != 'n') {
+			assign_node_values(nodes[i], &nodes[i-1]->val, &nodes[i+1]->val, 0);
+			set_binary_operation(nodes[i], value[0]);
+		}
 	}
+
+	//evaluate nodes in accordance with ORDEROPS
+	Node *result_node;
+	for(i = 1; i < ORDEROPSLEN; i++) //skip n, which are already evaluated
+	{
+		int j;
+		for(j = 0; j < tokens_size[0]; j++)
+		{
+			if(nodes[j]->type == ORDEROPS[i]) {
+				eval_node(nodes[j]);
+				result_node = nodes[j];
+			}
+		}
+	}
+
+	double result = result_node->val;
+	free(nodes);
+
+	printf("RESULT: %f\n", result);
 
 	return 0;
 }
--- expression_engine/expression_engine.h
+++ expression_engine/expression_engine.h
@@ -6,7 +6,13 @@
 #include <string.h>
 #include <stdbool.h>
 
-const char OPERATIONS[] = {'+', '-', '*', '/'};
+const char L1BINOPS[] = {'+', '-'};
+const char L2BINOPS[] = {'/', '*'};
+const int OPSLEN[] = {2, 2};
+
+const char ORDEROPS[] = {'n', '2', '1'};
+const int ORDEROPSLEN = 3;
+
 const int TOKEN_LENGTH = 5;
 const int TOKEN_AMOUNT = 10;
 
