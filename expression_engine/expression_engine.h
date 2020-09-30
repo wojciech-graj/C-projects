0155e17c32dd7479c414963c0d72cd19ed511f30
Wojjy72 <wojtek.graj.2004@gmail.com>
2020-09-30 21:13:12 +0200

    Make global constants static

--- expression_engine/expression_engine.h
+++ expression_engine/expression_engine.h
@@ -10,19 +10,19 @@
 typedef struct Node Node;
 typedef struct ListNode ListNode;
 
-const char *L5OPS[] = {"+", "-"};
-const char *L4OPS[] = {"/", "*", "%"};
-const char *L3OPS[] = {"^"};
-const char *L2OPS[] = {"!", "ln", "log", "sqrt", "sin", "cos", "tan", "sinh", "cosh", "tanh"};
-const char *L1OPS[] = {"(", ")", "x", "y"};
-const char *L0OPS[] = {"pi", "e", "phi"};
-const int OPSLEN[] = {3, 4, 10, 1, 3, 2};
-const double L0OPVALS[] = {M_PI, M_E, (1+sqrt(5))/2};
-const char OPTYPES[] = {'c', '\0', '2', '3', '4', '5'};
-const int NUMOPTYPES = 6;
+static const char *L5OPS[] = {"+", "-"};
+static const char *L4OPS[] = {"/", "*", "%"};
+static const char *L3OPS[] = {"^"};
+static const char *L2OPS[] = {"!", "ln", "log", "sqrt", "sin", "cos", "tan", "sinh", "cosh", "tanh"};
+static const char *L1OPS[] = {"(", ")", "x", "y"};
+static const char *L0OPS[] = {"pi", "e", "phi"};
+static const int OPSLEN[] = {3, 4, 10, 1, 3, 2};
+static const double L0OPVALS[] = {M_PI, M_E, (1+sqrt(5))/2};
+static const char OPTYPES[] = {'c', '\0', '2', '3', '4', '5'};
+static const int NUMOPTYPES = 6;
 
-const int TOKEN_LENGTH = 6;
-const int TOKEN_AMOUNT = 30;
+static const int TOKEN_LENGTH = 6;
+static const int TOKEN_AMOUNT = 30;
 
 int tokenize(char (*tokens)[TOKEN_AMOUNT][TOKEN_LENGTH], char input[]);
 bool string_in_array(char *value, const char *array[], const int length);
