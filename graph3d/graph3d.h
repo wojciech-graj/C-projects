ce1c89fc5acecb86e47f971a8c5249595d787bfe
Wojjy72 <wojtek.graj.2004@gmail.com>
2020-10-09 20:08:31 +0200

    Add AdaptiveColor struct. Include float.h

--- graph3d/graph3d.h
+++ graph3d/graph3d.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 #include <GL/gl.h>
 #include <GL/glut.h>
@@ -14,7 +15,12 @@
 #include "../expression_engine/math_functions.h"
 
 typedef struct ConfigData ConfigData;
+typedef struct AdaptiveColor AdaptiveColor;
 
 const int BUFFER_SIZE = 255;
+const float BLACK[3] = {0.0, 0.0, 0.0};
+const float RED[3] = {1.0, 0.0, 0.0};
+const float GREEN[3] = {0.0, 1.0, 0.0};
+const float BLUE[3] = {0.0, 0.0, 1.0};
 
 #endif
