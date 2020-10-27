f0c15397d2415bed546843729b30ec02bc4ee038
Wojjy72 <wojtek.graj.2004@gmail.com>
2020-10-27 18:57:54 +0100

    Add basic isometric rendering

--- marble_game/marble-game.c
+++ marble_game/marble-game.c
@@ -3,8 +3,25 @@
 #include <GL/glu.h>
 #include <assert.h>
 
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
 SDL_Window *window;
 SDL_GLContext main_context;
+
+int cur_row = 0;
+
+int level[] = {
+	3, 3, 3, 3, 3, 3,
+	  3, 3, 3, 3, 3, 3,
+	3, 3, 4, 4, 3, 3,
+	  3, 3, 4, 4, 3, 3,
+	3, 3, 3, 3, 3, 3,
+	  3, 3, 3, 3, 3, 3,
+	3, 3, 3, 3, 3, 3};
+
+int level_width = 6;
+int level_height = 7;
 
 void init()
 {
@@ -13,7 +30,8 @@ void init()
 	window = SDL_CreateWindow("MARBLE GAME",
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		640, 480, SDL_WINDOW_OPENGL);
+		WINDOW_WIDTH, WINDOW_HEIGHT,
+		SDL_WINDOW_OPENGL);
 	assert(window);
 
 	main_context = SDL_GL_CreateContext(window);
@@ -27,6 +45,7 @@ void init()
     glDepthFunc(GL_LEQUAL);
     glShadeModel(GL_SMOOTH);
     glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	gluOrtho2D(0, 10, 0, 10);
 }
 
 void quit()
@@ -36,23 +55,37 @@ void quit()
 	SDL_Quit();
 }
 
+void draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	int x, y;
+	glBegin(GL_TRIANGLES);
+		for(y = 1; y < level_height - 1; y++)
+			for(x = 1; x < level_width - 1; x++)
+			{
+				glColor3f(0, 1, y % 2);
+				int x_offset = (y % 2 == 0); //TODO:Rewrite better
+				glVertex2f(x - .5 + x_offset/ 2., level[y * level_width + x + x_offset - 1] - y/4.); //L
+				glVertex2f(x + x_offset/ 2., level[(y - 1) * level_width + x] - y/4. + .25); //T
+				glVertex2f(x + x_offset/ 2., level[(y + 1) * level_width + x] - y/4. - .25); //B
+
+				glVertex2f(x + x_offset/ 2., level[(y - 1) * level_width + x] - y/4. + .25); //T
+				glVertex2f(x + x_offset/ 2., level[(y + 1) * level_width + x] - y/4. - .25); //B
+				glVertex2f(x + .5 + x_offset/ 2., level[y * level_width + x + x_offset] - y/4.); //R
+
+			}
+	glEnd();
+
+	SDL_GL_SwapWindow(window);
+}
+
 int main(int argc, char *argv[])
 {
 	init();
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-		glColor3f(1, 0, 0);
-		glVertex2f(0, 0);
-		glColor3f(0, 1, 0);
-		glVertex2f(0, 1);
-		glColor3f(0, 0, 1);
-		glVertex2f(1, 1);
-	glEnd();
+	draw();
 
-	SDL_GL_SwapWindow(window);
-
-	sleep(5);
+	sleep(10);
 
 	quit();
 }
