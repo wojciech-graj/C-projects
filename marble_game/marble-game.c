5a2e6e53af34452a3fa90f73e2c06a9c20713201
Wojjy72 <wojtek.graj.2004@gmail.com>
2020-10-28 12:59:25 +0100

    Rework renderer

--- marble_game/marble-game.c
+++ marble_game/marble-game.c
@@ -6,23 +6,23 @@
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
+#define level_width 4
+#define level_height 4
 
 SDL_Window *window;
 SDL_GLContext main_context;
 
 int cur_row = 0;
 
-int level[] = {
-	0, 4, 3, 8, 8, 3,
-	  4, 3, 8, 8, 8, 3,
-	4, 4, 3, 8, 8, 3,
-	  4, 3, 3, 3, 3, 3,
-	3, 3, 3, 3, 3, 3,
-	  3, 3, 3, 3, 3, 3,
-	0, 3, 3, 3, 3, 3};
+int level[][4] = {
+	{3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3},
+			{3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3},
+	{3, 3, 3, 3}, {3, 5, 5, 3}, {3, 3, 3, 3}, {6, 6, 6, 6},
+			{3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 3, 3}};
 
-int level_width = 6;
-int level_height = 7;
+float level_projection[level_width * level_height][4];
+
+enum dir{l, t, r, b};
 
 void init()
 {
@@ -75,81 +75,69 @@ void draw()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	int x, y;
-	for(y = 1; y < level_height - 1; y++)
+	for(y = 0; y < level_height; y++)
 	{
 		int offset = y % 2;
 		glColor3f(1, 1, offset);
-		for(x = 0; x < level_width - 1; x++)
+		for(x = 0; x < level_width; x++)
 		{
-			int sq_l = level[y * level_width + x];
-			int sq_r = level[y * level_width + x + 1];
-			int sq_t = level[(y - 1) * level_width + x + offset];
-			int sq_b = level[(y + 1) * level_width + x + offset];
-			if(sq_t - sq_b >= 2) sq_t = sq_b;
-			if(sq_b - sq_t >= 2) sq_b = sq_t;
-			if(sq_l - sq_r >= 2) sq_l = sq_t;
-			if(sq_r - sq_l >= 2) sq_r = sq_t;
+			float *tile = level_projection[y * level_width + x];
 			float x_l = x - .5 + offset/2.;
 			float x_m = x + offset/2.;
 			float x_r = x + .5 + offset/2.;
-			float y_l = sq_l/2. - y/4.;
-			float y_t = sq_t/2. - y/4. + .25;
-			float y_r = sq_r/2. - y/4.;
-			float y_b = sq_b/2. - y/4. - .25;
 
-			int slope_sign = (signbit(y_l - y_r) ?  -1 : 1);
+			int slope_sign = (signbit(tile[l] - tile[r]) ?  -1 : 1);
 
-			float c_l = .5 + ((y_l - y_b) / 3) * slope_sign; //color
-			float c_r = .5 + ((y_t - y_r) / 3) * slope_sign;
-
+			//top surface
+			float c_l = .5 + ((tile[l] - tile[b]) / 3) * slope_sign; //color
+			float c_r = .5 + ((tile[t] - tile[r]) / 3) * slope_sign;
 			glBegin(GL_TRIANGLES);
 			glColor3f(c_l, c_l, c_l);
-			triangle(x_m, y_b,
-				x_l, y_l,
-				x_m, y_t); //left triangle
+			triangle(x_m, tile[b],
+				x_l, tile[l],
+				x_m, tile[t]); //left triangle
 			glColor3f(c_r, c_r, c_r);
-			triangle(x_m, y_b,
-				x_r, y_r,
-				x_m, y_t); // right triangle
+			triangle(x_m, tile[b],
+				x_r, tile[r],
+				x_m, tile[t]); // right triangle
 			glEnd();
 
+			//outline
 			glColor3f(0, 0, 0);
 			glBegin(GL_LINE_LOOP);
-			quad(x_l, y_l,
-				x_m, y_t,
-				x_r, y_r,
-				x_m, y_b);
+			quad(x_l, tile[l],
+				x_m, tile[t],
+				x_r, tile[r],
+				x_m, tile[b]);
 			glEnd();
 
-			if(y < level_height - 3)
-			{
-				if(sq_r - level[(y+2) * level_width + x + 1] >= 2) {
-					glColor3f(0, 1, 0);
-					glBegin(GL_QUADS);
-					quad(x_r, y_r,
-						x_m, y_b,
-						x_m, level[(y + 3) * level_width + x + offset]/2. - y/4. - .25,
-						x_r, level[(y + 2) * level_width + x + 1]/2. - y/4.);
-					glEnd();
-				}
-				if(sq_l - level[(y+2) * level_width + x] >= 2) {
-					glColor3f(0, 1, 1);
-					glBegin(GL_QUADS);
-					quad(x_l, y_l,
-						x_m, y_b,
-						x_m, level[(y + 3) * level_width + x+ offset]/2. - y/4. - .25,
-						x_l, level[(y + 2) * level_width + x - 1]/2. - y/4.);
-					glEnd();
-				}
-			}
+			//
 		}
 	}
 	SDL_GL_SwapWindow(window);
 }
 
+void calculate_projection()
+{
+	int y,x;
+	for(y = 0; y < level_height; y++)
+	{
+		for(x = 0; x < level_width; x++)
+		{
+			int i = y * level_width + x;
+			level_projection[i][l] = level[i][l]/2. - y/4.;
+			level_projection[i][t] = level[i][t]/2. - y/4. + .25;
+			level_projection[i][r] = level[i][r]/2. - y/4.;
+			level_projection[i][b] = level[i][b]/2. - y/4. - .25;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	init();
+
+	calculate_projection();
 
 	draw();
 
